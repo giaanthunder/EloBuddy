01889591fc9168afd64ee52aa6151e1568401191
giaanthunder <giaan086.nvktd@gmail.com>
2016-09-04 02:02:14 +0700

    new nidalee

---
+++ Nidalee/Nidalee.cs
@@ -0,0 +1,14 @@
+﻿using EloBuddy.SDK.Events;
+
+namespace Nidalee
+{
+    class Nidalee
+    {
+        static void Main(string[] args)
+        {
+            {
+                Loading.OnLoadingComplete += More.Loading_OnLoadingComplete;
+            }
+        }
+    }
+}
